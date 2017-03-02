6dba351932d0e6fa0daa1e117b20b172c48231c3
浮生若水 <betterfrank@163.com>
2017-03-02 15:07:31 +0800

    first

---
+++ application/logs/log-2016-09-09.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-09-09 07:30:19 --> Severity: Warning --> mysqli::real_connect():  F:\aci\system\database\drivers\mysqli\mysqli_driver.php 161
+ERROR - 2016-09-09 07:30:19 --> Unable to connect to the database
+ERROR - 2016-09-09 07:31:52 --> Severity: Warning --> mysqli::real_connect():  F:\aci\system\database\drivers\mysqli\mysqli_driver.php 161
+ERROR - 2016-09-09 07:31:52 --> Unable to connect to the database
+ERROR - 2016-09-09 07:31:53 --> Severity: Warning --> mysqli::real_connect():  F:\aci\system\database\drivers\mysqli\mysqli_driver.php 161
+ERROR - 2016-09-09 07:31:53 --> Unable to connect to the database
+ERROR - 2016-09-09 07:31:54 --> Severity: Warning --> mysqli::real_connect():  F:\aci\system\database\drivers\mysqli\mysqli_driver.php 161
+ERROR - 2016-09-09 07:31:54 --> Unable to connect to the database
